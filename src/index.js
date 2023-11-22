5f9c1d3a53e74f01841afd3085f13d548c53bcee
Linda Reyes <lindamreyesr@gmail.com>
2023-11-21 23:04:51 -0500

    Funcion mdLink

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
-module.exports = () => {
-  // ...
-};
+const fs = require('fs');
+const path = require('path'); 
+
+const mdLinks = (path, options) => {
+  return new Promise((resolve, reject) => {
+    // identifica si la ruta existe
+    if (fs.existsSync(path)) {
+
+
+      // Resuelve la promesa con algún resultado
+      resolve(/* resultado */);
+    }else {
+      // si no existe la Ruta rechaza la promesa con un mensaje error
+      reject('La ruta no existe')
+    }
+  })
+}
+
+module.exports = mdLinks;
--- test/md-links.spec.js
+++ test/md-links.spec.js
@@ -1,4 +1,4 @@
-const mdLinks = require('../');
+const mdLinks = require('../src/index');
 
 
 describe('mdLinks', () => {
@@ -7,4 +7,19 @@ describe('mdLinks', () => {
     console.log('FIX ME!');
   });
 
+});
+
+describe('mdLinks', () => {
+  it('debería ser una función', () => {
+    expect(typeof mdLinks).toBe('function');
+  });
+  //it('debería devolver una promesa', () => {
+  //  expect (mdLinks()).toBe(typeof Promise);
+  //});
+  it('debe rechazar cuando el path no existe ', () => {
+    //expect(typeof mdLinks).toBe('Promise');
+    return mdLinks('/documentos/prueba/noexiste.md').catch((error) => {
+      expect(error).toBe("La ruta no existe");
+    });
+  });
 });
