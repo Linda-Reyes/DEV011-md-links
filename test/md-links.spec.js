5fd6d87cf6c5b7ec25005b4dff71df9d98a80864
Linda Reyes <lindamreyesr@gmail.com>
2023-12-05 23:28:42 -0500

    corregir test hito 1

--- test/md-links.spec.js
+++ test/md-links.spec.js
@@ -1,11 +1,13 @@
 const mdLinks = require('../src/index');
+const path = require('path');
 const {
+  isAbsolutePath,
   convertAbsolute,
   pathExists,
   validMdextension,
   readFileMd,
-  findLinks,
-} = require('./functions');
+} = require('../src/functions');
+
 
 describe('mdLinks', () => {
   it('should...', () => {
@@ -23,34 +25,72 @@ describe('mdLinks', () => {
     expect(result).toBeInstanceOf(Promise);
   }); 
     
-  it('debe rechazar cuando el path no existe ', () => {
-    //expect(typeof mdLinks).toBe('Promise');
-    return mdLinks('/documentos/prueba/noexiste.md').catch((error) => {
-      expect(error).toBe("La ruta no existe");
-    });
-  });
-});
-
-
-
-describe('mdLinks', () => {
-  test('Debería retornar una promesa que resuelve a un array de links', () => {
-    // Puedes realizar pruebas aquí para mdLinks sin validar los enlaces
+  it('Debería retornar una promesa que resuelve a un array de links', () => {
     const path = 'README.md';
 
     return mdLinks(path).then((links) => {
       expect(links).toEqual(expect.any(Array));
-      // Añade más aserciones según tus necesidades
     });
   });
 
-  test('Debería retornar una promesa que resuelve a un array de links con validación', () => {
-    // Puedes realizar pruebas aquí para mdLinks con validación de enlaces
+  it('Debería retornar una promesa que resuelve a un array de links con validación', () => {
     const path = 'README.md';
 
     return mdLinks(path, true).then((links) => {
       expect(links).toEqual(expect.any(Array));
     });
   });
-
 });
+
+describe('isAbsolutePath', () => {
+  test('Debería retornar true para rutas absolutas', () => {
+    const result = isAbsolutePath('C:/Users/juan/Desktop/LABORATORIA/DEV011-md-links/README.md');
+    expect(result).toBe(true);
+  });
+
+  test('Debería retornar false para rutas relativas', () => {
+    const result = isAbsolutePath('README.md');
+    expect(result).toBe(false);
+  });
+});
+
+describe('convertAbsolute', () => {
+  test('Debería retornar la ruta absoluta si ya es absoluta', () => {
+    const ruta = 'C:/Users/juan/Desktop/LABORATORIA/DEV011-md-links/README.md';
+    const result = convertAbsolute(ruta);
+    expect(result).toBe(ruta);
+  });
+
+  test('Debería retornar la ruta convertida a absoluta si es relativa', () => {
+    const rutaRelativa = 'README.md';
+    const rutaAbsoluta = path.resolve(rutaRelativa);
+    const result = convertAbsolute(rutaRelativa);
+    expect(result).toBe(rutaAbsoluta);
+  });
+});
+
+describe('pathExists', () => {
+  test('Debería resolver para rutas existentes', () => {
+    const rutaExistente = 'README.md';
+    return expect(pathExists(rutaExistente)).resolves.toBe('La ruta existe');
+  });
+
+  test('Debería rechazar para rutas que no existen', () => {
+    const rutaNoExistente = '.README.xyz';
+    return expect(pathExists(rutaNoExistente)).rejects.toThrow('La ruta no existe');
+  });
+});
+
+describe('validMdextension', () => {
+  test('Debería resolver para archivos Markdown válidos', () => {
+    const archivoMd = 'README.md';
+    return expect(validMdextension(archivoMd)).resolves.toBe('Es un archivo Markdown');
+  });
+
+  test('Debería rechazar para archivos que no son Markdown', () => {
+    const archivoNoMd = 'archivo.txt';
+    return expect(validMdextension(archivoNoMd)).rejects.toThrow('No es un archivo Markdown');
+  });
+});
+
+
