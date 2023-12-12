fa1867afae1eb3b370c02e718f147679a205e34d
Linda Reyes <lindamreyesr@gmail.com>
2023-12-12 08:45:00 -0500

    function getStats

--- src/functions.js
+++ src/functions.js
@@ -99,6 +99,16 @@ function validateLinks(links) {
   return Promise.all(linkPromises);
 }
 
+function getStats(links) {
+  const totalLinks = links.length;
+  const uniqueLinks = [...new Set(links.map(link => link.href))].length;
+
+  return {
+    total: totalLinks,
+    unique: uniqueLinks,
+  };
+}
+
  module.exports = {
    isAbsolutePath,
    convertAbsolute,
@@ -107,5 +117,7 @@ function validateLinks(links) {
    readFileMd,
    findLinks,
    validateLinks,
+   getStats
  };
+
  
